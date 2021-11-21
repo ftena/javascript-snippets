b1f961ba6c7443038fab483d192caa6d1ab38e76
Fran Tena <tarod.net@gmail.com>
2021-11-21 10:16:20 +0100

    new sample

--- basic/regular-expressions.js
+++ basic/regular-expressions.js
@@ -20,6 +20,26 @@ console.log(result);
 // Use the shorthand character class \w to count the number of alphanumeric characters in various quotes and strings.
 // \w. This shortcut is equal to [A-Za-z0-9_]
 quoteSample = "The five boxing wizards jump quickly.";
-let alphabetRegexV2 = /\w/g; // Change this line
+let alphabetRegexV2 = /\w/g; 
 result = quoteSample.match(alphabetRegexV2).length;
+console.log(result);
+
+// Match Everything But Letters and Numbers
+// \W. This shortcut is the same as [^A-Za-z0-9_].
+quoteSample = "The five boxing wizards jump quickly.";
+let nonAlphabetRegex = /\W/g; 
+result = quoteSample.match(nonAlphabetRegex).length;
+console.log(result);
+
+/* Restrict Possible Usernames
+    Usernames are used everywhere on the internet. They are what give users a unique identity on their favorite sites.
+    You need to check all the usernames in a database. Here are some simple rules that users have to follow when creating their username.
+    1. Usernames can only use alpha-numeric characters.
+    2. The only numbers in the username have to be at the end. There can be zero or more of them at the end. Username cannot start with the number.
+    3. Username letters can be lowercase and uppercase.
+    4. Usernames have to be at least two characters long. A two-character username can only use alphabet letters as characters.
+*/
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/i;
+result = userCheck.test(username);
 console.log(result);
