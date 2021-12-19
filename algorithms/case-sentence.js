4abd20ca361f084cf0fb754462cd31211b964a7c
Fran Tena <tarod.net@gmail.com>
2021-12-19 13:21:45 +0100

    new example

---
+++ algorithms/case-sentence.js
@@ -0,0 +1,13 @@
+// Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+
+function titleCase(str) {
+  var convertToArray = str.toLowerCase().split(" ");
+  var result = convertToArray.map(function(val) {
+    return val.replace(val.charAt(0), val.charAt(0).toUpperCase());
+  });
+  return result.join(" ");
+}
+
+console.log(titleCase("I'm a little tea pot"));
+console.log(titleCase("sHoRt AnD sToUt"));
+console.log(titleCase("HERE IS MY HANDLE HERE IS MY SPOUT"));
