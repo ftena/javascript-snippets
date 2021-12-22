e1d6a5563a914d4278a8f8c2ee026d07339a49f3
Fran Tena <tarod.net@gmail.com>
2021-12-22 20:27:32 +0100

    new example about Object Oriented Programming

---
+++ basic/object-oriented-programming.js
@@ -0,0 +1,8 @@
+// Object using this keyword
+let duck = {
+  name: "Aflac",
+  numLegs: 2,
+  sayName: function() {return "The name of this duck is " + this.name + ".";}
+};
+
+console.log(duck.sayName());
