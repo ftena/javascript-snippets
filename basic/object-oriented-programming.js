3646d90abc1870476f5bd038437b0151d248d9cd
Fran Tena <tarod.net@gmail.com>
2021-12-26 15:44:10 +0100

    new code for the example

--- basic/object-oriented-programming.js
+++ basic/object-oriented-programming.js
@@ -6,3 +6,27 @@ let duck = {
 };
 
 console.log(duck.sayName());
+
+// Using constructor to create objetcs
+function Bird() {
+   this.name = "Albert";
+   this.color  = "blue";
+   this.numLegs = 2;
+}
+
+let blueBird = new Bird();
+console.log(blueBird);
+console.log("instance of Bird? " + (blueBird instanceof Bird));
+
+// Receive some arguments
+function Dog(name, color) {
+	  this.name = name;
+	  this.color  = color;
+	  this.numLegs = 4;
+}
+
+let terrier = new Dog("Bro", "black");
+console.log(terrier);
+console.log("instance of Bird? " + (terrier instanceof Dog));
+
+
