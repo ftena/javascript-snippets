f0a466e661eac71007026e00215b5791065ddbb9
Fran Tena <tarod.net@gmail.com>
2022-01-01 23:01:19 +0100

    update sample

--- exercises/split-join.js
+++ exercises/split-join.js
@@ -5,3 +5,12 @@ function sentensify(str) {
 console.log(sentensify("May-the-force-be-with-you"));
 console.log(sentensify("The.force.is.strong.with.this.one"));
 console.log(sentensify("There,has,been,an,awakening"));
+
+// It converts a string title and returns the hyphenated version for the URL
+function urlSlug(title) {
+  return (title.split(/\W/).filter(data => data.length != 0).join("-").toLowerCase());
+}
+
+console.log(urlSlug("Winter Is Coming"));
+console.log(urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone"));
+
