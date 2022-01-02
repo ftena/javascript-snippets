256361a19ba9c25e09b1b9e73c31f79d7ad59fff
Fran Tena <tarod.net@gmail.com>
2022-01-02 12:45:34 +0100

    update algorithm

--- algorithms/caesar-cipher.js
+++ algorithms/caesar-cipher.js
@@ -1,16 +1,9 @@
 function rot13(str) {
-	  let alphabet = [];
+	for (let i = 0; i < str.length; i++) {
+		console.log(str[i] + " : " + String.fromCharCode((str[i].charCodeAt() - 13) % 13 + 65));
+	}
 
-	  for(let ascii = 65; ascii < 91; ascii++)
-		  {
-		      console.log("a) " + String.fromCharCode(ascii));
-
-		  console.log("b) " + String.fromCharCode( ( (0+13) % 26) + ascii ) );
-
-		      console.log("b) " + String.fromCharCode((ascii+13) % 26 + ascii));
-		    }
-
-	  return str;
+	return str;
 }
 
-rot13("SERR PBQR PNZC");
+rot13("NOZ");
