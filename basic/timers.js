5728728480598c66a6aed77d388b095ede9eff93
Fran Tena <tarod.net@gmail.com>
2022-01-23 08:51:14 +0100

    use setInterval()

--- basic/timers.js
+++ basic/timers.js
@@ -14,3 +14,16 @@ const secondParam = 20;
 console.log("wait three seconds...")
 // runs after 3 seconds
 setTimeout(myFunction, 3000, firstParam, secondParam);
+
+// setInterval()
+// similar to setTimeout, with a difference: instead of running the callback function once, it will run it forever, at the specific time interval you specify (in milliseconds)
+
+var value = 0;
+
+const myFunction2 = (increment) => {
+   value = value + increment;
+   console.log("setInterval: " + value);
+};
+
+setInterval(myFunction2, 1000, 10);
+
