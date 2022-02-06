d32fcde0dad9bab40d054dcc0ad9913cbf42bec0
Fran Tena <tarod.net@gmail.com>
2022-02-06 12:31:03 +0100

    more promises

---
+++ basic/promises-more-examples.js
@@ -0,0 +1,50 @@
+// from: https://nodejs.dev/learn/understanding-javascript-promises
+
+// A promise is commonly defined as a proxy for a value that will eventually
+// become available.
+// Async functions use promises behind the scenes, so understanding how
+// promises work is fundamental to understanding how async and await work.
+let done = true
+
+const isItDoneYet = new Promise((resolve, reject) => {
+  if (done) {
+    const workDone = 'Here is the thing I built'
+    resolve(workDone)
+  } else {
+    const why = 'Still working on something else'
+    reject(why)
+  }
+})
+
+const checkIfItsDone = () => {
+  isItDoneYet
+    .then(ok => {
+      console.log(ok)
+    })
+    .catch(err => {
+      console.error(err)
+    })
+}
+
+checkIfItsDone()
+
+// Because we've created the promise in the above snippet,
+// it has already started executing. 
+const fs = require('fs')
+
+const getFile = (fileName) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(fileName, (err, data) => {
+      if (err) {
+        reject(err)  // calling `reject` will cause the promise to fail with or without the error passed as an argument
+        return        // and we don't want to go any further
+      }
+      resolve(data)
+    })
+  })
+}
+
+getFile('/etc/passwd')
+.then(data => console.log(data))
+.catch(err => console.error(err))
+
